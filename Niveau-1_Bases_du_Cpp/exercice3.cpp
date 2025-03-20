2345f1aade560b9d65b0fd4af751ddc578527523
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-20 16:10:22 +0100

    Exercice 3 Done.

---
+++ Niveau-1_Bases_du_Cpp/exercice3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//3. Lire un nombre entier entré par l'utilisateur et l'afficher.  
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Entrez un nombre entier : ";
+    cin >> x;
+    cout << "Your number is : " << x;
+    return 0;
+}
