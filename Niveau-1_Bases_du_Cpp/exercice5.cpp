9e5704e882699e603f04f9b90e1ac12f56b62f38
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-24 15:15:59 +0100

    Exercice 5 Done.

---
+++ Niveau-1_Bases_du_Cpp/exercice5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//5. Utiliser une constante (`const`) pour stocker une valeur et l’afficher.  
+using namespace std;
+
+int main() {
+    const float noCHANGE = 3.14;
+    cout << noCHANGE;
+    return 0;
+}
