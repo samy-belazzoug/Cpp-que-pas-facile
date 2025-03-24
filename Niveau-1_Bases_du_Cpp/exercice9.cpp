dd1762060f691e2f028a66cacd6379594432e453
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-24 15:50:16 +0100

    Exercice 9 Done.

---
+++ Niveau-1_Bases_du_Cpp/exercice9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//9. Afficher les nombres de 1 à 10 avec une boucle `for`.  
+using namespace std;
+
+int main() {
+    //for (condition de départ; jusqu'a ? ; itération)
+    for (int i=0;i<11;i++) {
+        cout << i << endl;
+    }
+}
