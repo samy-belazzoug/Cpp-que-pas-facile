1280cce79967f3577534ac19e36f806cd953c022
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-24 15:55:13 +0100

    Exercice 10 Done.

---
+++ Niveau-1_Bases_du_Cpp/exercice10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//10. Afficher les nombres pairs entre 1 et 20 avec une boucle `while`.  
+using namespace std;
+
+int main() {
+    int i = 0;
+    while (i < 21) {
+    cout << i << endl;
+    i += 2; }
+    return 0;
+}
