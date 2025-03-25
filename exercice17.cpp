b70c905387133b7a299913a5608db6278361f05f
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-25 12:38:37 +0100

    Exercice 17 Done.

---
+++ exercice17.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+//17. Générer un nombre aléatoire entre 1 et 100.  
+using namespace std;
+
+int main() {
+    /* De base, randomNum prends une valeur au hasard
+    et reste la même à chaque fois que l'on lance le code.
+    Avec srand(time(0)), la valeur de randomNum change à chaque
+    fois que l'on lance le code.*/ 
+    srand(time(0)); 
+    int randomNum = rand() % 10;
+    cout << randomNum;
+    return 0;
+}
